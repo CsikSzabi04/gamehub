1bd7d6c1c0830bd2a95f757427438aa8395c8d0b
CsikSzabi04 <68150404+CsikSzabi04@users.noreply.github.com>
2025-03-09 20:54:04 +0100

    Upgrade for Live responsive for mobile

--- src/Live.jsx
+++ src/Live.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "tailwindcss";
 
-export default function EsportsMatches() {
+export default function Live() {
   const [matches, setMatches] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -29,10 +29,10 @@ export default function EsportsMatches() {
   return (
     <section id="matches" className="mb-8">
       <h2 className="text-2xl font-semibold mb-4">Latest Esports Matches</h2>
-      <div className="carousel-container overflow-hidden p-10">
-        <div className="carousel flex p-10" style={{ transform: `translateX(-${currentIndex * 20}%)`, transition: "transform 1s ease-in-out" }}>
+      <div className="carousel-container overflow-hidden px-4 sm:px-10">
+        <div className="carousel flex transition-transform duration-1000 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
           {matches.map((match) => (
-            <div key={match.id} className="game-card carousel-item flex-none border-cyan-950">
+            <div key={match.id} className="game-card carousel-item flex-none w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
               <div className="p-4 rounded-md shadow-md min-h-[20%] max-h-[80%] flex flex-col justify-between h-full">
                 <div className="relative mb-4">
                   <h3 className="text-lg font-bold mb-2">{match.tournament.name}</h3>
@@ -53,9 +53,9 @@ export default function EsportsMatches() {
                 <div className="flex-grow mt-10"></div>
               </div>
               <div className="float-start">
-                <p className="text-xs text-gray-500 mt-2"> {`Start Time: ${new Date(match.startTimestamp * 1000).toLocaleString()}`}</p>
-                <p className="text-xs text-gray-500 mt-3"> {`Best Of: ${match.bestOf}`}</p>
-                <a href={`https://www.esports.com/tournament/${match.slug}`} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm mt-3 block mt-auto"> View Match Details</a>
+                <p className="text-xs text-gray-500 mt-2">{`Start Time: ${new Date(match.startTimestamp * 1000).toLocaleString()}`}</p>
+                <p className="text-xs text-gray-500 mt-3">{`Best Of: ${match.bestOf}`}</p>
+                <a href={`https://www.esports.com/tournament/${match.slug}`} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm mt-3 block mt-auto">View Match Details</a>
               </div>
             </div>
           ))}
