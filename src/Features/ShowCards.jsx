10d0c3370ea539b74f5388855f2fab264acbb051
CsikSzabi04 <68150404+CsikSzabi04@users.noreply.github.com>
2025-03-20 21:55:20 +0100

    ShowCards responsive for mobile

--- src/Features/ShowCards.jsx
+++ src/Features/ShowCards.jsx
@@ -54,27 +54,30 @@ export default function ShowCards({ selectedGame, closeModal, modalVisible }) {
 
   return (
     <div className="modal show fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50" id="game-modal">
-      <div className="modal-content rounded-lg sm:max-w-lg mx-4 sm:mx-0 sm:p-8 overflow-y-auto">
-        <div className='inp flex center justify center'>
-          <span className="close-button font-bold text-white absolute top-6 right-7 cursor-pointer" onClick={closeModal}>&times;</span>
-          {fav==true ? <span className="add-button text-white rounded-md cursor-pointer top-5 left-6" onClick={addFav}>Add to Fav</span>:<span className="close-button text-white rounded-md cursor-pointer top-5 left-6" onClick={delFav}>Delete from Fav</span>}
-        </div>
-        {error && <> <br /><br /> <p className="error text-red-500 text-sm mt-2">{error}</p></>}
-        <img src={selectedGame.background_image} alt={selectedGame.name} className="rounded-lg mb-4 mt-8 object-cover w-full sm:h-80" />
-        <h2 className="text-2xl sm:text-3xl font-bold mb-4">{selectedGame.name}</h2>
-        <p className="text-sm sm:text-base">Release Date: {selectedGame.released || "?"}</p>
-        <p className="text-sm sm:text-base">Rating: {selectedGame.rating || "?"}/5</p>
-        <p className="text-sm sm:text-base">Stores: {selectedGame.stores ? selectedGame.stores.map(store => store.store.name).join(", ") : "?"}</p>
-        <p className="text-sm sm:text-base">Platforms: {selectedGame.platforms ? selectedGame.platforms.map(p => p.platform.name).join(", ") : "?"}</p>
-        <div className="row mt-2">
-          <p className="text-sm sm:text-base">Genres:
-            <div id="tag" className="text-xs sm:text-sm text-gray-500">
-              {selectedGame.tags ? selectedGame.tags.map(g => g.name).join(", ") : "?"}
-            </div>
-          </p>
-        </div>
+    <div className="modal-content rounded-lg sm:max-w-lg mx-4 sm:mx-0 sm:p-8 overflow-y-auto">
+      <div className='inp flex center justify center'>
+        <span className="close-button font-bold text-white absolute top-1 right-2 cursor-pointer" onClick={closeModal}>&times;</span>
+        {fav == true ? 
+          <span className="add-button text-white rounded-md cursor-pointer top-5 left-6" onClick={addFav}>Add to Fav</span> :
+          <span className="close-button text-white rounded-md cursor-pointer top-5 left-6" onClick={delFav}>Delete from Fav</span>}
+      </div>
+      {error && <> <br /><br /> <p className="error text-red-500 text-sm mt-2">{error}</p></>}
+      <img src={selectedGame.background_image} alt={selectedGame.name} className="rounded-lg mb-4 mt-8 object-cover w-full sm:h-80" />
+      <h2 className="text-2xl sm:text-3xl font-bold mb-4">{selectedGame.name}</h2>
+      <p className="text-sm sm:text-base">Release Date: {selectedGame.released || "?"}</p>
+      <p className="text-sm sm:text-base">Rating: {selectedGame.rating || "?"}/5</p>
+      <p className="text-sm sm:text-base">Stores: {selectedGame.stores ? selectedGame.stores.map(store => store.store.name).join(", ") : "?"}</p>
+      <p className="text-sm sm:text-base">Platforms: {selectedGame.platforms ? selectedGame.platforms.map(p => p.platform.name).join(", ") : "?"}</p>
+      <div className="row mt-2">
+        <p className="text-sm sm:text-base">Genres:
+          <div id="tag" className="text-xs sm:text-sm text-gray-500">
+            {selectedGame.tags ? selectedGame.tags.map(g => g.name).join(", ") : "?"}
+          </div>
+        </p>
       </div>
     </div>
+  </div>
+  
 
   );
 }
