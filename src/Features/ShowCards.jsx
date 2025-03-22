57ac080a65f53ba85d335a23a435f40401a0d3d2
CsikSzabi04 <68150404+CsikSzabi04@users.noreply.github.com>
2025-03-22 22:00:05 +0100

    Update ShowCards.jsx

--- src/Features/ShowCards.jsx
+++ src/Features/ShowCards.jsx
@@ -66,9 +66,9 @@ export default function ShowCards({ selectedGame, closeModal, modalVisible }) {
   if (!selectedGame) return null;
 
   return (
-    <div className="modal show fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50" id="game-modal">
+  <div className="modal show fixed inset-0 bg-black bg-opacity-75 flexz-50" id="game-modal">
       <div className="modal-content rounded-lg sm:max-w-lg mx-4 sm:mx-0 sm:p-8 overflow-y-auto max-h-screen sm:max-h-[80vh]">
-        <div className='inp flex center justify-center'>
+        <div className='inp flex '>
           <span className="close-button font-bold text-white absolute top-1 right-2 cursor-pointer" onClick={closeModal}>&times;</span>
           {fav == true ? 
             <span className="add-button text-white rounded-md cursor-pointer top-5 left-6" onClick={addFav}>Add to Fav</span> :
